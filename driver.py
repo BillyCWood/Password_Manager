a35ad42e5c79f267e693fff78e117430e1438cd2
BillyCWood <61811789+BillyCWood@users.noreply.github.com>
2021-10-09 01:07:50 -0600

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,5 +1,17 @@
 ###Author: Billy Wood
 import login
+import addInformation
+
+
+dirPath = "C:/Users/billy/CompSci/PythonProg/PasswordProject/users"
+
+
+
+
+
+def main():
+    login.main()
+    #addInformation.main()
 
 if __name__ == "__main__":
-    pass
+    main()
